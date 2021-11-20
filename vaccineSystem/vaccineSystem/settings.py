9bd1fac7d8ed43d27ff5a982160452e41ae251ab
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-20 02:54:58 +0100

    Add main, user app to installed apps, set new languaje

--- vaccineSystem/vaccineSystem/settings.py
+++ vaccineSystem/vaccineSystem/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    "apps.main",
+    "apps.users",
 ]
 
 MIDDLEWARE = [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "es-es"
 
 TIME_ZONE = 'UTC'
 
