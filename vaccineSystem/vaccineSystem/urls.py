bf3724e12d917ed243cb5014b223ddd339c4c3c9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-20 03:28:47 +0100

    Remove / from apps urls

--- vaccineSystem/vaccineSystem/urls.py
+++ vaccineSystem/vaccineSystem/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Apps URLS
-    path("/", include("apps.main.urls")),
-    path("/", include("apps.users.urls")),
+    path("", include("apps.main.urls")),
+    path("", include("apps.users.urls")),
 ]
 
 if settings.DEBUG:  
