9f40a6c8e16f21eb5ea7a30a6d5895c86c37017b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-20 03:38:15 +0100

    Recieve data from post login method

--- vaccineSystem/apps/main/views.py
+++ vaccineSystem/apps/main/views.py
@@ -10,6 +10,10 @@ def homeView(request):
 
 def loginView(request):
     context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
     return render(request, 'login.html', context)
 
 
