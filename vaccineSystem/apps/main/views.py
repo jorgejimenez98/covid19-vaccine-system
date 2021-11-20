70eb5b1d7c1a7087ecb2fd87fd071d85f1fcd429
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-20 03:21:03 +0100

    Init login, home and dashboard view function

--- vaccineSystem/apps/main/views.py
+++ vaccineSystem/apps/main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def homeView(request):
+    context = {}
+    if not request.user.is_authenticated:
+        return redirect('loginView')
+    return redirect('dashboardView')
+
+
+def loginView(request):
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def dashboardView(request):
+    context = {}
+    return render(request, 'home.html', context)
