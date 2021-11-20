51b35a0ccf7546373341eaacc6a39a89f16f46d2
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-20 03:28:06 +0100

    Change / place

--- vaccineSystem/apps/main/urls.py
+++ vaccineSystem/apps/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.homeView, name="homeView"),
-    path("/login", views.loginView, name="loginView"),
-    path("/dashboard", views.dashboardView, name="dashboardView"),
+    path("", views.homeView, name="homeView"),
+    path("login/", views.loginView, name="loginView"),
+    path("dashboard/", views.dashboardView, name="dashboardView"),
 ]
