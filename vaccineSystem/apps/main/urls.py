30ca695d39a9515175f90a29cd2220689f96273b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-20 03:21:13 +0100

    Add login, home and dashboard view to urls

--- vaccineSystem/apps/main/urls.py
+++ vaccineSystem/apps/main/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("/", views.homeView, name="homeView"),
+    path("/login", views.loginView, name="loginView"),
+    path("/dashboard", views.dashboardView, name="dashboardView"),
 ]
