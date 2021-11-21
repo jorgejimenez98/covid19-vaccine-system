cdc9a1ac960d6f87dbadf83c7443d62f067c8c6f
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-21 03:00:40 +0100

    Add url user delete

--- vaccineSystem/apps/users/urls.py
+++ vaccineSystem/apps/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("users/list/", views.userListView, name="userListView")
+    path("users/list/", views.userListView, name="userListView"),
+    path("users/delete/<int:pk>/", views.userDeleteView, name="userDeleteView")
 ]
