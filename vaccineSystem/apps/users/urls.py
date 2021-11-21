9f9cedde6c73aa3d115b2f62637e51bbbcfbc8c9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-21 03:13:35 +0100

    Add users add url

--- vaccineSystem/apps/users/urls.py
+++ vaccineSystem/apps/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("users/list/", views.userListView, name="userListView"),
-    path("users/delete/<int:pk>/", views.userDeleteView, name="userDeleteView")
+    path("users/add/", views.userAddView, name="userAddView"),
+    path("users/delete/<int:pk>/", views.userDeleteView, name="userDeleteView"),
 ]
