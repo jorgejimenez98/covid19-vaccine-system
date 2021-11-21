b001c908bf9d9e3290accadb451c35911ee43257
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-21 03:16:06 +0100

    Add comments to all view user functions

--- vaccineSystem/apps/users/views.py
+++ vaccineSystem/apps/users/views.py
@@ -11,25 +11,34 @@ from .formclass import UserForm
 @login_required(login_url='/login')
 @checkUserAccess(rol='ADMIN', error_url='/403')
 def userListView(request):
+    # Init Context
     context = {"users": get_user_model().objects.all()}
+    # Render List
     return render(request, 'users/list.html', context)
 
 
-
+""" ADD USER """
 @login_required(login_url='/login')
 @checkUserAccess(rol='ADMIN', error_url='/403')
 def userAddView(request):
+    # Init Context
     context = {"user": UserForm("", "", "", "", "")}
+    # Render User Form
     return render(request, 'users/add.html', context)
 
 
+""" DELETE USER """
 @login_required(login_url='/login')
 @checkUserAccess(rol='ADMIN', error_url='/403')
 def userDeleteView(request, pk=None):
+    # Get user data from template
     user = get_user_model().objects.get(pk=pk)
+    # Validate if there is onle an Admin user
     if user.is_staff and get_user_model().objects.filter(is_staff=True).count() == 1:
         messages.error(request, getNoAdminDeleteMessage(user.username))
     else:
+        # Delete user after validation
         user.delete()
         messages.error(request, deleteSuccessMessage(user.username))
+    # Render to users list
     return redirect("userListView")
