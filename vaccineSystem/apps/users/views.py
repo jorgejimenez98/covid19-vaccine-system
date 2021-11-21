c5ecfd6e7af6f3cb5b67c2f59e09bc47905e4b54
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-21 03:01:07 +0100

    Create user delete view function

--- vaccineSystem/apps/users/views.py
+++ vaccineSystem/apps/users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from ..main.decorators import checkUserAccess
+from ..main.errorFunctions import *
 
 
 @login_required(login_url='/login')
@@ -9,3 +11,15 @@ from ..main.decorators import checkUserAccess
 def userListView(request):
     context = {"users": get_user_model().objects.all()}
     return render(request, 'users/list.html', context)
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def userDeleteView(request, pk=None):
+    user = get_user_model().objects.get(pk=pk)
+    if user.is_staff and get_user_model().objects.filter(is_staff=True).count() == 1:
+        messages.error(request, getNoAdminDeleteMessage(user.username))
+    else:
+        user.delete()
+        messages.error(request, deleteSuccessMessage(user.username))
+    return redirect("userListView")
