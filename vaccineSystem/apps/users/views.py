f0ca0886af283b360120cb7223d94e809b8fd429
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-21 01:33:37 +0100

    Create user list view function

--- vaccineSystem/apps/users/views.py
+++ vaccineSystem/apps/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from ..main.decorators import checkUserAccess
 
-# Create your views here.
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def userListView(request):
+    context = {"users": get_user_model().objects.all()}
+    return render(request, 'users/list.html', context)
