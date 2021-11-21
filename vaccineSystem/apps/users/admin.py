c69d7beefc5dcfb4ad225cf70574f8a6f989b902
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-21 01:12:02 +0100

    Add user model to admin

--- vaccineSystem/apps/users/admin.py
+++ vaccineSystem/apps/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from django.utils.translation import gettext as _
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['username', 'name']
+    fieldsets = (
+        (None, {'fields': ('username', 'password',)}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser',
+                        'isSpecialist',)}
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2',),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
