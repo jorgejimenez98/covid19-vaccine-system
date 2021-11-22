fa968ad0aaeacc2b6731d9604395af6aa9bdd439
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-22 03:24:57 +0100

    Add province edit url

--- vaccineSystem/apps/locality/urls.py
+++ vaccineSystem/apps/locality/urls.py
@@ -5,5 +5,6 @@ from .views import provinceViews as pviews
 urlpatterns = [
     # Province urls
     path("provinces/list/", pviews.provinceListView, name='provinceListView'),
-    path("provinces/add/", pviews.provinceAddView, name='provinceAddView')
+    path("provinces/add/", pviews.provinceAddView, name='provinceAddView'),
+    path("provinces/edit/<int:pk>/", pviews.provinceEditView, name='provinceEditView'),
 ]
