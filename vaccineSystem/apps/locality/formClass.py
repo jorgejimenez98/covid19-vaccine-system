1a11e78f2b0d119c52df6c6b1776093af330c353
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-22 02:52:31 +0100

    Create province, municipality form context class

---
+++ vaccineSystem/apps/locality/formClass.py
@@ -0,0 +1,51 @@
+class ProvinceForm:
+    def __init__(self, id, name):
+        self.__id = id
+        self.__name = name
+
+     @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+    
+
+class MunicipalityForm:
+    def __init__(self, id, name, provinceId):
+        self.__id = id
+        self.__name = name
+        self.__provinceId = provinceId
+
+     @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def provinceId(self):
+        return self.__provinceId
+
+    @provinceId.setter
+    def provinceId(self, value):
+        self.__provinceId = value
