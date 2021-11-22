e6c88e3a3662a7311a422a7b8940de05b0f08607
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-22 04:04:48 +0100

    Create mun add view function

--- vaccineSystem/apps/locality/views/municipalityViews.py
+++ vaccineSystem/apps/locality/views/municipalityViews.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from apps.main.decorators import checkUserAccess
 from apps.main.errorFunctions import *
 from ..formClass import MunicipalityForm
-from ..models import Municipality
+from ..models import Municipality, Province
 
 
 """ Municipality LIST """
@@ -19,3 +19,37 @@ def municipalityListView(request):
     context = {"municipalities": Municipality.objects.all().order_by('-pk')}
     # Render List
     return render(request, 'municipality/list.html', context)
+
+
+""" ADD Municipality """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def municipalityAddView(request):
+    # Init Context
+    context = {
+        "mun": MunicipalityForm(None, "", "", ""),
+        "provinces": Province.objects.all().order_by('-pk')
+    }
+    # Render Mun Form
+    if request.method == 'POST':
+        # Get data from template
+        name = request.POST.get("val_name")
+        val_province_id = request.POST.get("val_province_id")
+        # Update context
+        context['mun'].name = name
+        context['mun'].provinceId = val_province_id
+        try:
+            # Create Mun
+            Municipality.objects.create(
+                name=name,
+                province=Province.objects.get(pk=int(val_province_id))
+            )
+            # Redirect MUN List
+            messages.success(request, getSuccessCreatedMessage('Municipio'))
+            return redirect("municipalityListView")
+        except Exception as e:
+            # Validate all posible errors
+            messages.error(request, e.args[0])
+    return render(request, 'municipality/add.html', context)
