f32e8530e237e543ed8adfa84d3fca62bb63648e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-22 03:16:57 +0100

    Create add province view function

--- vaccineSystem/apps/locality/views/provinceViews.py
+++ vaccineSystem/apps/locality/views/provinceViews.py
@@ -18,3 +18,32 @@ def provinceListView(request):
     context = {"provinces": Province.objects.all().order_by('-pk')}
     # Render List
     return render(request, 'province/list.html', context)
+
+
+""" ADD PROVINCE """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def provinceAddView(request):
+    # Init Context
+    context = {"province": ProvinceForm(None, "")}
+    # Render Province Form
+    if request.method == 'POST':
+        # Get data from template
+        name = request.POST.get("val_name")
+        # Update context
+        context['province'].name = name
+        try:
+            # Create user
+            Province.objects.create(name=name)
+            # Redirect User List
+            messages.success(request, getSuccessCreatedMessage('Provincia'))
+            return redirect("provinceListView")
+        except IntegrityError:
+            # Validate Unique username field
+            messages.error(request, getUniqueTypeError('Provincia', name))
+        except Exception as e:
+            # Validate all posible errors
+            messages.error(request, e.args[0])
+    return render(request, 'province/add.html', context)
