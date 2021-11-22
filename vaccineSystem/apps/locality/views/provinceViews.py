9ae36b4d035353e525d9fd9d1bd43a08a5bca561
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-22 03:25:14 +0100

    Create province edit view function

--- vaccineSystem/apps/locality/views/provinceViews.py
+++ vaccineSystem/apps/locality/views/provinceViews.py
@@ -47,3 +47,36 @@ def provinceAddView(request):
             # Validate all posible errors
             messages.error(request, e.args[0])
     return render(request, 'province/add.html', context)
+
+
+""" EDIT USER """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def provinceEditView(request, pk):
+    # Get Province to Edit
+    province = Province.objects.get(pk=pk)
+    provinceForm = ProvinceForm(province.pk, province.name)
+    # Init Context
+    context = {"province": provinceForm}
+    # Render Province Form
+    if request.method == 'POST':
+        # Get data from template
+        name = request.POST.get("val_name")
+        # Update context
+        context['province'].name = name
+        try:
+            # Edit user
+            province.name = name
+            province.save()
+            # Redirect User List
+            messages.success(request, getSuccessEditMessage('Provincia'))
+            return redirect("provinceListView")
+        except IntegrityError:
+            # Validate Unique username field
+            messages.error(request, getUniqueTypeError('Provincia', name))
+        except Exception as e:
+            # Validate all posible errors
+            messages.error(request, e.args[0])
+    return render(request, 'province/add.html', context)
