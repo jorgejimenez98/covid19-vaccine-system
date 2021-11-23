33715ac874a9044fabef7f14f93f649ed317ecfa
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 01:34:55 +0100

    Create isProvinceId template tag

--- vaccineSystem/apps/main/templatetags/template_tags.py
+++ vaccineSystem/apps/main/templatetags/template_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter(name='isRol')
 def isRol(user, rol):
     return user.rol == rol
+
+@register.filter(name='isProvinceId')
+def isProvinceId(province, provinceId):
+    return province.pk == provinceId
