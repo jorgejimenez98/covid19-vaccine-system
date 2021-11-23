23b5dbc8dd8f8df3afcdec09565ba51d50b301d6
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 03:08:40 +0100

    Add consulting room to admin panel

--- vaccineSystem/apps/locality/admin.py
+++ vaccineSystem/apps/locality/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Province, Municipality, School, Polyclinic
+from .models import Province, Municipality, School, Polyclinic, ConsultingRoom
 
 admin.site.register(Province)
 admin.site.register(Municipality)
 admin.site.register(School)
 admin.site.register(Polyclinic)
+admin.site.register(ConsultingRoom)
