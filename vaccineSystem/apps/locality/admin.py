80aa7220279b30d33a69cab23e377dc9458a290d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 01:45:50 +0100

    Add school model to admin

--- vaccineSystem/apps/locality/admin.py
+++ vaccineSystem/apps/locality/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Province, Municipality
+from .models import Province, Municipality, School
 
 admin.site.register(Province)
 admin.site.register(Municipality)
+admin.site.register(School)
