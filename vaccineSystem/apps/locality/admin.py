bef7205fbcf5e7b1247a054eb036fee6f0a3c315
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 02:28:31 +0100

    Add Polyclinic to admin panel

--- vaccineSystem/apps/locality/admin.py
+++ vaccineSystem/apps/locality/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Province, Municipality, School
+from .models import Province, Municipality, School, Polyclinic
 
 admin.site.register(Province)
 admin.site.register(Municipality)
 admin.site.register(School)
+admin.site.register(Polyclinic)
