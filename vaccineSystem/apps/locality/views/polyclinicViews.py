b3aa305e2d6299d952f735962b81a10bf6165cb4
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 02:43:00 +0100

    Add pol add view function

--- vaccineSystem/apps/locality/views/polyclinicViews.py
+++ vaccineSystem/apps/locality/views/polyclinicViews.py
@@ -19,3 +19,37 @@ def polyclinicListView(request):
     context = {"polyclinics": Polyclinic.objects.all().order_by('-pk')}
     # Render List
     return render(request, 'polyclinic/list.html', context)
+
+
+""" ADD SCHOOLS """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def polyclinicAddView(request):
+    # Init Context
+    context = {
+        "pol": SchoolForm(None, "", "", ""),
+        "muns": Municipality.objects.all().order_by('-pk')
+    }
+    # Render Mun Form
+    if request.method == 'POST':
+        # Get data from template
+        name = request.POST.get("val_name")
+        val_mun_id = request.POST.get("val_mun_id")
+        # Update context
+        context['pol'].name = name
+        context['pol'].munId = val_mun_id
+        try:
+            # Create Schools
+            Polyclinic.objects.create(
+                name=name,
+                municipality=Municipality.objects.get(pk=int(val_mun_id))
+            )
+            # Redirect MUN List
+            messages.success(request, getSuccessCreatedMessage('Policlinico'))
+            return redirect("polyclinicListView")
+        except Exception as e:
+            # Validate all posible errors
+            messages.error(request, e.args[0])
+    return render(request, 'polyclinic/addOrEdit.html', context)
