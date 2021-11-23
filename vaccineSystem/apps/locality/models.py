b8b4250aaaa75b42de58afab9d16345a700bfdcc
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 01:43:14 +0100

    Add mun atribute to school model

--- vaccineSystem/apps/locality/models.py
+++ vaccineSystem/apps/locality/models.py
@@ -16,8 +16,11 @@ class Municipality(models.Model):
     def __str__(self):
         return f'Municipio {self.name} - {self.province.name}'
 
+
 class School(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    municipality = models.ForeignKey(
+        Municipality, on_delete=models.PROTECT, related_name='schools')
 
     def __str__(self):
         return f'Escuela {self.name}'
