8ee5137451257a1d57e447b1b07ccb5612c0cbfe
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-23 02:12:29 +0100

    Create add school view function

--- vaccineSystem/apps/locality/views/schoolViews.py
+++ vaccineSystem/apps/locality/views/schoolViews.py
@@ -9,7 +9,7 @@ from ..formClass import SchoolForm
 from ..models import Municipality, School
 
 
-""" Municipality LIST """
+""" SCHOOLS LIST """
 
 
 @login_required(login_url='/login')
@@ -19,3 +19,37 @@ def schoolListView(request):
     context = {"schools": School.objects.all().order_by('-pk')}
     # Render List
     return render(request, 'school/list.html', context)
+
+
+""" ADD SCHOOLS """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def schoolAddView(request):
+    # Init Context
+    context = {
+        "school": SchoolForm(None, "", "", ""),
+        "muns": Municipality.objects.all().order_by('-pk')
+    }
+    # Render Mun Form
+    if request.method == 'POST':
+        # Get data from template
+        name = request.POST.get("val_name")
+        val_mun_id = request.POST.get("val_mun_id")
+        # Update context
+        context['school'].name = name
+        context['school'].munId = val_mun_id
+        try:
+            # Create Schools
+            School.objects.create(
+                name=name,
+                municipality=Municipality.objects.get(pk=int(val_mun_id))
+            )
+            # Redirect MUN List
+            messages.success(request, getSuccessCreatedMessage('Escuela'))
+            return redirect("schoolListView")
+        except Exception as e:
+            # Validate all posible errors
+            messages.error(request, e.args[0])
+    return render(request, 'school/addOrdEdit.html', context)
