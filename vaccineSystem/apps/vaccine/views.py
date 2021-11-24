eb94823730e9bbad0c8f232f51b40fbe4d7d0e90
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-24 23:36:16 +0100

    Create vaccine list view function

--- vaccineSystem/apps/vaccine/views.py
+++ vaccineSystem/apps/vaccine/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from ..main.decorators import checkUserAccess
+from ..main.errorFunctions import *
+from .models import Vaccine
 
-# Create your views here.
+
+""" VACCINE LIST """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='ADMIN', error_url='/403')
+def vaccinesListView(request):
+    # Init Context
+    context = {"vaccines": Vaccine.objects.all().order_by('-pk')}
+    # Render to vaccine List
+    return render(request, 'vaccines/list.html', context)
