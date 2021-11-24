aa6d6db4edabda9d33c364f18381ca5d83743d0a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-25 00:35:10 +0100

    Add update values in vaccine form

--- vaccineSystem/apps/vaccine/formClass.py
+++ vaccineSystem/apps/vaccine/formClass.py
@@ -1,7 +1,14 @@
 class VaccineForm:
-    def __init__(self, id, name, healthPersonnel, canConsultingRoom, canSchool):
+    def __init__(self, id=None, name="", healthPersonnel=False, canConsultingRoom=False, canSchool=False):
         self.id = id
         self.name = name
         self.healthPersonnel = healthPersonnel
         self.canConsultingRoom = canConsultingRoom
         self.canSchool = canSchool
+
+    def updateValues(self, vaccine):
+        self.id = vaccine.id
+        self.name = vaccine.name
+        self.healthPersonnel = vaccine.healthPersonnel
+        self.canConsultingRoom = vaccine.canConsultingRoom
+        self.canSchool = vaccine.canSchool
