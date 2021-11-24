1610dddcf0b639d561e9d13937c328fc8379dbd7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-25 00:39:58 +0100

    Add vaccine delete url

--- vaccineSystem/apps/vaccine/urls.py
+++ vaccineSystem/apps/vaccine/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("vaccines/list", views.vaccinesListView, name="vaccinesListView"),
     path("vaccines/add", views.vaccinesAddView, name="vaccinesAddView"),
     path("vaccines/edit/<int:pk>", views.vaccinesEditView, name="vaccinesEditView"),
+    path("vaccines/delete/<int:pk>/", views.vaccinesDeleteView, name="vaccinesDeleteView"),
 ]
