eb16a6ccd3ea6cef3ecc0af2f1ffae1e3d7f3ff8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-25 00:27:08 +0100

    Add vaccine add url

--- vaccineSystem/apps/vaccine/urls.py
+++ vaccineSystem/apps/vaccine/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("vaccines/list", views.vaccinesListView, name="vaccinesListView"),
+    path("vaccines/add", views.vaccinesAddView, name="vaccinesAddView"),
 ]
