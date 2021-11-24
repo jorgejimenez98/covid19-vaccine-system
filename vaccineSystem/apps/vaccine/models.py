18ddc7d10639a19065b75124197d2e388c7aeb2e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-24 23:18:13 +0100

    Create vaccine model

--- vaccineSystem/apps/vaccine/models.py
+++ vaccineSystem/apps/vaccine/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=255)
+    healthPersonnel = models.BooleanField(default=False)
+    canConsultingRoom = models.BooleanField(default=False)
+    canSchool = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Vacuna {self.name}'
+
+    def getPlace(self):
+        if self.healthPersonnel:
+            return 'Personal de Salud'
+        elif self.canConsultingRoom:
+            return 'Es por consultorio'
+        else:
+            return 'Es por escuela'
