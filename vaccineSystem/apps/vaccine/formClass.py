e3a543e76eaf3468ee8365c10efedf2e2f9979e6
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-25 00:26:31 +0100

    Create vaccine form class for template context

---
+++ vaccineSystem/apps/vaccine/formClass.py
@@ -0,0 +1,7 @@
+class VaccineForm:
+    def __init__(self, id, name, healthPersonnel, canConsultingRoom, canSchool):
+        self.id = id
+        self.name = name
+        self.healthPersonnel = healthPersonnel
+        self.canConsultingRoom = canConsultingRoom
+        self.canSchool = canSchool
