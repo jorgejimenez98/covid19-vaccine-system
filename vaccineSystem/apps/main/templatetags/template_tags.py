c296104f0ac9824266d0c802aea447ae412c2ae8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-26 00:30:55 +0100

    Create is sex tempalte tag

--- vaccineSystem/apps/main/templatetags/template_tags.py
+++ vaccineSystem/apps/main/templatetags/template_tags.py
@@ -7,6 +7,12 @@ register = template.Library()
 def isRol(user, rol):
     return user.rol == rol
 
+
 @register.filter(name='isProvinceId')
 def isProvinceId(province, provinceId):
     return province.pk == provinceId
+
+
+@register.filter(name='isSex')
+def isSex(person, sex):
+    return person.sex == sex
