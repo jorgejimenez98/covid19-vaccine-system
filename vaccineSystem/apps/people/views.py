ee6bee73bd30b317eb7a22a3eec17876c72740cf
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-25 03:03:39 +0100

    Create person list view function

--- vaccineSystem/apps/people/views.py
+++ vaccineSystem/apps/people/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import IntegrityError
+from django.db.models import ProtectedError
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from ..main.decorators import checkUserAccess
+from ..main.errorFunctions import *
+from .models import People
 
-# Create your views here.
+
+""" Consulting Room LIST """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='SPECIALIST', error_url='/403')
+def peopleListView(request):
+    # Init Context
+    context = {"persons": People.objects.all()}
+    # Render List
+    return render(request, 'people/list.html', context)
