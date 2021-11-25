3628cefd10feac6b3de38fbfbc373859e749b62a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-26 00:31:06 +0100

    Create person form class for context

---
+++ vaccineSystem/apps/people/formClass.py
@@ -0,0 +1,26 @@
+from .models import People
+
+
+class PersonForm:
+    def __init__(self, ci=None, name="", last_names="", sex="", age="", address="", consulting_room_id="", consulting_room_name="", positive_pcr=False, date_pcr=""):
+        self.ci = ci
+        self.name = name
+        self.last_names = last_names
+        self.sex = sex
+        self.age = age
+        self.address = address
+        self.consulting_room_id = consulting_room_id
+        self.consulting_room_name = consulting_room_name
+        self.positive_pcr = positive_pcr
+        self.date_pcr = date_pcr
+
+    def updateValues(self, people: People):
+        self.ci = people.ci
+        self.name = people.name
+        self.last_names = people.last_names
+        self.sex = people.sex
+        self.address = people.address
+        self.consulting_room_id = people.consulting_room.pk
+        self.consulting_room_name = people.consulting_room.name
+        self.positive_pcr = people.positive_pcr
+        self.date_pcr = str(people.date_pcr)
