91c6803c6710722501622f3af546380fe2203a61
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-25 02:35:19 +0100

    Add all vaccination types to admin panel

--- vaccineSystem/apps/vaccine/admin.py
+++ vaccineSystem/apps/vaccine/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Vaccine
+from .models import Vaccine, ConsultingRoom_Vaccination, School_Vaccination, PersonalHealth_Vaccination
 
 admin.site.register(Vaccine)
+admin.site.register(PersonalHealth_Vaccination)
+admin.site.register(School_Vaccination)
+admin.site.register(ConsultingRoom_Vaccination)
