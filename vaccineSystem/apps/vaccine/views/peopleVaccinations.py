32b0903af89ca8e178562967982c61807f3cdee0
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-26 02:45:50 +0100

    Init person vaccines List view function

---
+++ vaccineSystem/apps/vaccine/views/peopleVaccinations.py
@@ -0,0 +1,27 @@
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from django.db.models import ProtectedError
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from apps.main.decorators import checkUserAccess
+from apps.main.errorFunctions import *
+from ..models import Vaccine
+from apps.people.models import People
+from ..formClass import VaccineForm
+
+
+""" VACCINE LIST """
+
+
+@login_required(login_url='/login')
+@checkUserAccess(rol='SPECIALIST', error_url='/403')
+def personVaccinesList(request, pk):
+    person = People.objects.get(pk=pk)
+    # Init Context
+    context = {
+        "person": person,
+        "vaccines": person.vaccinations.all(),
+    }
+    # Render to vaccine List
+    return render(request, 'vaccines/personVaccines.html', context)
