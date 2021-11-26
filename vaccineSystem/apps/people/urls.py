5823564226d66eb49d3120c64def701c33b42deb
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-26 02:14:36 +0100

    Add edit people url

--- vaccineSystem/apps/people/urls.py
+++ vaccineSystem/apps/people/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('persons/list', views.peopleListView, name='peopleListView'),
     path('persons/add', views.peopleAddView, name='peopleAddView'),
     path('persons/delete/<int:pk>/', views.peopleDeleteView, name='peopleDeleteView'),
+    path('persons/edit/<int:pk>/', views.peopleEditView, name='peopleEditView'),
 ]
