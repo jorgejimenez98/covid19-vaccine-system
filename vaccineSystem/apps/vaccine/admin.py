a0a2dcd7872af9571a4796d9878636873a61cb6a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-26 02:22:49 +0100

    Add Doce model to admin panel

--- vaccineSystem/apps/vaccine/admin.py
+++ vaccineSystem/apps/vaccine/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Vaccine, ConsultingRoom_Vaccination, School_Vaccination, PersonalHealth_Vaccination
+from .models import Vaccine, ConsultingRoom_Vaccination, School_Vaccination, PersonalHealth_Vaccination, Doce
 
 admin.site.register(Vaccine)
 admin.site.register(PersonalHealth_Vaccination)
 admin.site.register(School_Vaccination)
 admin.site.register(ConsultingRoom_Vaccination)
+admin.site.register(Doce)
