bde1433b1cce2c8c55b500aee72a4f29457b7e30
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-29 21:57:03 +0100

    Create school vaccination form class

--- vaccineSystem/apps/vaccine/formClass.py
+++ vaccineSystem/apps/vaccine/formClass.py
@@ -12,3 +12,17 @@ class VaccineForm:
         self.healthPersonnel = vaccine.healthPersonnel
         self.canConsultingRoom = vaccine.canConsultingRoom
         self.canSchool = vaccine.canSchool
+
+
+class SchoolVaccineForm:
+    def __init__(self, id=None, vaccineId='', badReaction=False, schoolId=""):
+        self.id = id
+        self.vaccineId = vaccineId
+        self.badReaction = badReaction
+        self.schoolId = schoolId
+
+    def updateValues(self, vaccination):
+        self.id = vaccination.pk
+        self.vaccineId = vaccination.vaccine.id
+        self.badReaction = vaccination.has_adverse_reactions
+        self.schoolId = vaccination.school.id
