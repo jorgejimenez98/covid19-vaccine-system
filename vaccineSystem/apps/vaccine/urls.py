b35d6d2ffd83b43171a03e626453914e6fe51dff
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 06:09:57 +0100

    Add doces list and insert urls

--- vaccineSystem/apps/vaccine/urls.py
+++ vaccineSystem/apps/vaccine/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import vaccineViews as vviews
 from .views import peopleVaccinations as pviews
+from .views import docesViews as dviews 
 
 urlpatterns = [
     path("vaccines/list", vviews.vaccinesListView, name="vaccinesListView"),
@@ -14,4 +15,9 @@ urlpatterns = [
     path("person/vaccines/add/school/<int:pk>", pviews.personAddSchoolVaccine, name="personAddSchoolVaccine"),
     path("person/vaccines/add/consult/<int:pk>", pviews.personAddConsultVaccine, name="personAddConsultVaccine"),
     path("person/vaccines/add/health/<int:pk>", pviews.personAddHealthVaccine, name="personAddHealthVaccine"),
+
+    # Doses Views 
+    path("person/vaccines/doces/<int:pk>/<str:vaccine>/<str:vaccineId>/", dviews.personVaccinesDocesList, name="personVaccinesDocesList"),
+    path("person/vaccines/add/<int:pk>/<str:vaccine>/<str:vaccineId>/", dviews.personVaccinesDocesAdd, name="personVaccinesDocesAdd"),
+
 ]
