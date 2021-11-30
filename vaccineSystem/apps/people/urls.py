f4596d02cfc0b678faa7424c35a0ee7363bf5ade
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 07:13:26 +0100

    Add excel export url

--- vaccineSystem/apps/people/urls.py
+++ vaccineSystem/apps/people/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('persons/list', views.peopleListView, name='peopleListView'),
     path('persons/add', views.peopleAddView, name='peopleAddView'),
+    path('persons/export', views.peopleExportView, name='peopleExportView'),
     path('persons/delete/<int:pk>/', views.peopleDeleteView, name='peopleDeleteView'),
     path('persons/edit/<int:pk>/', views.peopleEditView, name='peopleEditView'),
 ]
