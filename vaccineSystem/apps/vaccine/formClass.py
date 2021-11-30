ed7ff25253777b63759cfbd69c465cf4dbde0933
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-30 04:14:23 +0100

    Create consultgin room vaccine form class

--- vaccineSystem/apps/vaccine/formClass.py
+++ vaccineSystem/apps/vaccine/formClass.py
@@ -26,3 +26,16 @@ class SchoolVaccineForm:
         self.vaccineId = vaccination.vaccine.id
         self.badReaction = vaccination.has_adverse_reactions
         self.schoolId = vaccination.school.id
+
+class ConsultginVaccineForm:
+    def __init__(self, id=None, vaccineId='', badReaction=False, consultingId=""):
+        self.id = id
+        self.vaccineId = vaccineId
+        self.badReaction = badReaction
+        self.consultingId = consultingId
+
+    def updateValues(self, vaccination):
+        self.id = vaccination.pk
+        self.vaccineId = vaccination.vaccine.id
+        self.badReaction = vaccination.has_adverse_reactions
+        self.consultingId = vaccination.consulting_rooms.id
