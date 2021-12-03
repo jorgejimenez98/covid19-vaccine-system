d40f470bd3bcb204a9c3006439fb899e24c01bc7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-03 19:59:39 +0100

    Update settings for database on postgres

--- vaccineSystem/vaccineSystem/settings.py
+++ vaccineSystem/vaccineSystem/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'vaccineSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vacunasCovid',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432'
     }
 }
+
 
 
 # Password validation
